7113455be7374b0179e7558c1e147f29f1de4e63
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2021-12-26 18:11:06 +0800

    updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/LoginActivity.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/LoginActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     TextView textView;
+    Button homepageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +28,17 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        homepageButton = findViewById(R.id.loginbutton);
+
+        homepageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, homepage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
