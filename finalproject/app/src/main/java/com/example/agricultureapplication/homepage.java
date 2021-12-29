5df779bf0f9cd1860a03ed8036920798787f34d1
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2021-12-29 13:20:43 +0800

    updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/homepage.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/homepage.java
@@ -1,10 +1,19 @@
 package com.example.agricultureapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class homepage extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +21,10 @@ public class homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
         getSupportActionBar().hide();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
     }
 }
