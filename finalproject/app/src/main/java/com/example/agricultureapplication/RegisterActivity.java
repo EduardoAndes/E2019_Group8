820a6dce9c9caa87f6722402a6bcb3f0ecbba906
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2022-01-18 16:52:16 +0800

    updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/RegisterActivity.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/RegisterActivity.java
@@ -1,18 +1,44 @@
 package com.example.agricultureapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
-    TextView textView;
+
+    EditText editText_Firstname, editText_Lastname, editText_Birthday, editText_Age, editText_Email, editText_Username, editText_Password;
+    String str_Firstname, str_Lastname, str_Birthday, str_Age, str_Email, str_Username, str_Password;
+    String url = "https://e2019cc107group8.000webhostapp.com/register.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editText_Firstname = findViewById(R.id.editText_Firstname);
+        editText_Lastname = findViewById(R.id.editText_Lastname);
+        editText_Birthday = findViewById(R.id.editText_Birthday);
+        editText_Age = findViewById(R.id.editText_Age);
+        editText_Email = findViewById(R.id.editText_Email);
+        editText_Username = findViewById(R.id.editText_Username);
+        editText_Password = findViewById(R.id.editText_Password);
 
         getSupportActionBar().hide();
 
@@ -26,5 +52,69 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void Register(View view){
+        if(editText_Firstname.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Firstname", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText_Lastname.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Lastname", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText_Birthday.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Birthday", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText_Age.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Age", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText_Email.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText_Username.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Username", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText_Password.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            str_Firstname = editText_Firstname.getText().toString().trim();
+            str_Lastname = editText_Lastname.getText().toString().trim();
+            str_Birthday = editText_Birthday.getText().toString().trim();
+            str_Age = editText_Age.getText().toString().trim();
+            str_Email = editText_Email.getText().toString().trim();
+            str_Username = editText_Username.getText().toString().trim();
+            str_Password = editText_Password.getText().toString().trim();
+
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(RegisterActivity.this, response,Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(RegisterActivity.this, error.getMessage().toString(),Toast.LENGTH_SHORT).show();
+                }
+            }
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+
+                    params.put("firstname", str_Firstname);
+                    params.put("lastname", str_Lastname);
+                    params.put("birthday",str_Birthday);
+                    params.put("age", str_Age);
+                    params.put("email", str_Email);
+                    params.put("username", str_Username);
+                    params.put("password", str_Password);
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+
+            requestQueue.add(request);
+
+        }
     }
 }
--- finalproject/app/src/main/java/com/example/agricultureapplication/firstFragment_home.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/firstFragment_home.java
@@ -14,11 +14,23 @@ import android.widget.Toast;
 
 public class firstFragment_home extends Fragment {
 
+    Button btn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_first_home, container, false);
 
+        btn = view.findViewById(R.id.btnToast);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "HELLO", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
