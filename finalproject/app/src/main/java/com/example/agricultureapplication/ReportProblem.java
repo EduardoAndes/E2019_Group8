ec2cdc994768b3030d6fd67d1dbcfb44240c9b3a
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2022-02-02 18:39:58 +0800

    Updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/ReportProblem.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/ReportProblem.java
@@ -1,16 +1,30 @@
 package com.example.agricultureapplication;
 
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReportProblem extends AppCompatActivity {
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reportproblem);
+
+        ImageView imageView = findViewById(R.id.ArrowBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportProblem.this, homepage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         getSupportActionBar().hide();
     }
