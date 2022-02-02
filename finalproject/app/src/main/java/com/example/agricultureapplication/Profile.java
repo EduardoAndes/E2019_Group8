984ebb65c34f36568f1d82b9d31cea8e514c1c60
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2022-02-02 18:04:30 +0800

    Updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/Profile.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/Profile.java
@@ -1,16 +1,30 @@
 package com.example.agricultureapplication;
 
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        ImageView imageView = findViewById(R.id.ArrowBack1);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, homepage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         getSupportActionBar().hide();
     }
