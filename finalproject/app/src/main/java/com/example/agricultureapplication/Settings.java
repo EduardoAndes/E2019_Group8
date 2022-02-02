6d6d01dae8866095b67a722e79343de95a539ae2
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2022-02-02 13:56:15 +0800

    Updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/Settings.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/Settings.java
@@ -1,16 +1,28 @@
 package com.example.agricultureapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-
 public class Settings extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ImageView imageView = findViewById(R.id.ArrowBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.this, homepage.class);
+            }
+        });
 
         getSupportActionBar().hide();
     }
