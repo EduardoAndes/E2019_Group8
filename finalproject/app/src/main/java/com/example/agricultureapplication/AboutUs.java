3d0c7631051e7b893aea4be89ea807e8d3371940
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2022-02-03 18:49:07 +0800

    Updated

--- finalproject/app/src/main/java/com/example/agricultureapplication/AboutUs.java
+++ finalproject/app/src/main/java/com/example/agricultureapplication/AboutUs.java
@@ -1,15 +1,31 @@
 package com.example.agricultureapplication;
 
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUs extends AppCompatActivity {
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ImageView imageView = findViewById(R.id.ArrowBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutUs.this, homepage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
         getSupportActionBar().hide();
     }
 }
